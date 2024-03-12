04f1ea6c3e039e5f83e58067836c532e0154f390
unknown <jordan.conot@gmail.com>
2024-03-12 22:21:28 +0100

    style: page project ok, just missing link

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -15,7 +15,7 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
     const arrow = `~~>`;
 
     return (
-        <section className='flex flex-col gap-10'>
+        <section className='flex flex-col gap-10 mt-16'>
             <div className='flex items-center justify-between'>
                 <h2 className='text-color-3 text-3xl font-medium lg:flex lg:items-center'>
                     <span className='text-color-1 text-3xl'>{isProjectsPage ? '/' : '#'}</span>{t('Main.h2')}
@@ -37,10 +37,10 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
                 </>
             )}
 
-            <div className='lg: flex flex-wrap justify-between gap-6'>
+            <div className='lg: flex flex-wrap justify-between gap-10'>
 
             
-            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[31%]'>
+            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
                 <div className='h-60 flex'>
                     <Image className='lg:h-60' src='/assets/img/sport-see.jpg' alt='Projet sport see' width={1920} height={1080} />
                 </div>
@@ -61,7 +61,7 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
                 </div>
             </div>
 
-            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[31%]'>
+            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
                 <div className='h-60 flex'>
                     <Image className='lg:h-60' src='/assets/img/sport-see.jpg' alt='Projet sport see' width={1920} height={1080} />
                 </div>
@@ -82,7 +82,7 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
                 </div>
             </div>
 
-            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[31%]'>
+            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
                 <div className='h-60 flex'>
                     <Image className='lg:h-60' src='/assets/img/les-petits-plats.jpg' alt='Projet Les petits plats' width={1920} height={1080}  />
                 </div>
@@ -107,7 +107,7 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
                 </div>
             </div>
 
-            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[31%]'>
+            <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
                 <div className='h-60 flex'>
                     <Image className='lg:h-60' src='/assets/img/oh-my-food.jpg' alt='Projet sport see' width={1920} height={1080}  />
                 </div>
@@ -133,8 +133,9 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
             {isProjectsPage && (
                 <>
                     <p className='text-color-3 text-3xl font-medium mt-8'><span className='text-color-1 text-3xl'>#</span>{t('Page-projects.small-projects')}</p>
-                    <div className='border-color-2 border flex flex-col lg:w-1/2'>
-                        <div className='w-full h-40 flex'>
+                    <div className='lg: flex flex-col justify-between gap-10 lg:flex-wrap lg:flex-row'>
+                    <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
+                        <div className='w-full h-40 hidden'>
                             <Image className='lg: w-full lg:h-80' src='/assets/img/sport-see.jpg' alt='Projet sport see' width={333} height={160} />
                         </div>
                         <div className='border-color-2 border flex p-2 border-x-0'>
@@ -154,8 +155,29 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
                         </div>
                     </div>
 
-                    <div className='border-color-2 border flex flex-col'>
-                        <div className='w-full h-40 flex'>
+                    <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
+                        <div className='w-full h-40 hidden'>
+                            <Image className='lg:w-full lg:h-80' src='/assets/img/sport-see.jpg' alt='Projet sport see' width={333} height={160} />
+                        </div>
+                        <div className='border-color-2 border flex p-2 border-x-0'>
+                            <p className='text-color-2 uppercase pl-2'>{t('Main.card.language')}</p>
+                        </div>
+                        <div className='flex flex-col gap-4 p-4'>
+                            <p className='uppercase text-color-3 text-2xl font-medium'>{t('Main.card.title')}</p>
+                            <p className='text-color-2'>{t('Main.card.description')}</p>
+                            <div className='flex flex-row gap-4'>
+                                <button className='text-color-3 border-color-1 border px-4 py-2 uppercase'>
+                                    {t('Main.card.btn-live')} {arrow}
+                                </button>
+                                <button className='text-color-3 border-color-1 border px-4 py-2 uppercase'>
+                                    {t('Main.card.btn-github')} {arrow}
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
+                        <div className='w-full h-40 hidden'>
                             <Image src='/assets/img/sport-see.jpg' alt='Projet sport see' width={333} height={160} />
                         </div>
                         <div className='border-color-2 border flex p-2 border-x-0'>
@@ -175,8 +197,8 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
                         </div>
                     </div>
 
-                    <div className='border-color-2 border flex flex-col'>
-                        <div className='w-full h-40 flex'>
+                    <div className='border-color-2 border flex flex-col lg:w-[47%] xl:w-[30%]'>
+                        <div className='w-full h-40 hidden'>
                             <Image src='/assets/img/sport-see.jpg' alt='Projet sport see' width={333} height={160} />
                         </div>
                         <div className='border-color-2 border flex p-2 border-x-0'>
@@ -194,6 +216,7 @@ const Projects: React.FC<ProjectsProps> = ({ isProjectsPage }) => {
                                 </button>
                             </div>
                         </div>
+                    </div>
                     </div>
                 </>
             )}
